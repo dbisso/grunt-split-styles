1b63992d6102c9d21db67722a62ecb5a33d2546c
Dan Bissonnet <dan@danisadesigner.com>
2014-02-25 19:01:55 +0000

    Remove boilderplate comments

--- tasks/split_styles.js
+++ tasks/split_styles.js
@@ -31,9 +31,6 @@ module.exports = function(grunt) {
 			pattern = options.pattern;
 		}
 
-		// Please see the Grunt documentation for more information regarding task
-		// creation: http://gruntjs.com/creating-tasks
-
 		// Our postCSS processor
 		var processor = postcss(function (css) {
 			if ( pattern.match ) {
@@ -85,7 +82,6 @@ module.exports = function(grunt) {
 
 		// Iterate over all specified file groups.
 		this.files.forEach(function(f) {
-			// Concat specified files.
 			var src = f.src.filter(function(filepath) {
 				// Warn on and remove invalid source files (if nonull was set).
 				if (!grunt.file.exists(filepath)) {
